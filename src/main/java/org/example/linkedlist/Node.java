ce0a5c47089bea828ef79880cb998571d32d3981
Arun <arunkumarb1521@gmail.com>
2024-06-21 14:12:26 +0530

    LInkedList-insert and show

---
+++ src/main/java/org/example/linkedlist/Node.java
@@ -0,0 +1,6 @@
+package org.example.linkedlist;
+
+public class Node {
+    int data;
+    Node next;
+}
---
+++ src/main/java/org/example/linkedlist/Runner.java
@@ -0,0 +1,16 @@
+package org.example.linkedlist;
+
+import java.util.LinkedList;
+
+public class Runner {
+    public static void main(String[] args) {
+        LInkedList list = new LInkedList();
+        list.insert(5);
+        list.insert(3);
+        list.insert(4);
+        list.insert(9);
+
+        list.show();
+
+    }
+}
