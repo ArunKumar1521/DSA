30a7e840e8f1126f4f5d834bc03a6de970ef504b
Arun <arunkumarb1521@gmail.com>
2024-06-24 11:35:42 +0530

    stack implementation using array

---
+++ src/main/java/org/example/stack/Stack.java
@@ -0,0 +1,29 @@
+package org.example.stack;
+
+public class Stack {
+    int[] stack = new int[5];
+    int top = 0;
+
+    public void push(int data){
+        stack[top] = data;
+        top++;
+    }
+    public void show(){
+        for(int n : stack){
+            System.out.print(n + " ");
+        }
+    }
+
+    public int pop(){
+        int data;
+        top--;
+        data = stack[top];
+        stack[top] = 0;
+        return data;
+    }
+    public int peek(){
+        int data;
+        data = stack[top-1];
+        return data;
+    }
+}
