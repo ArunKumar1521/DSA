4b0205e31000bb6de8dfc2e28c4814f08e0fa0c5
Arun <arunkumarb1521@gmail.com>
2024-06-24 12:27:00 +0530

    stack implementation using array-added methods

--- src/main/java/org/example/stack/Stack.java
+++ src/main/java/org/example/stack/Stack.java
@@ -5,25 +5,42 @@ public class Stack {
     int top = 0;
 
     public void push(int data){
-        stack[top] = data;
-        top++;
+        if(top == 5){
+            System.out.println("Stack is full");
+        } else {
+            stack[top] = data;
+            top++;
+        }
     }
     public void show(){
         for(int n : stack){
             System.out.print(n + " ");
         }
+        System.out.println();
     }
-
     public int pop(){
-        int data;
-        top--;
-        data = stack[top];
-        stack[top] = 0;
-        return data;
+        int data =0;
+        if(isEmpty()){
+            System.out.println("The stack is empty ");
+        } else {
+            top--;
+            data = stack[top];
+            stack[top] = 0;
+        }
+            return data;
+
     }
     public int peek(){
         int data;
         data = stack[top-1];
         return data;
     }
+    public void size(){
+         System.out.println("The size is : "+top);
+    }
+    public boolean isEmpty(){
+        return top <= 0;
+    }
+
+
 }
--- src/main/java/org/example/stack/runner1.java
+++ src/main/java/org/example/stack/runner1.java
@@ -3,14 +3,7 @@ package org.example.stack;
 public class runner1 {
     public static void main(String[] args) {
         Stack nums = new Stack();
-        nums.push(15);
-        nums.push(12);
-        System.out.println(nums.peek());
-        nums.push(13);
-        nums.push(14);
-
-        System.out.println(nums.pop());
         nums.show();
-
+        nums.pop();
     }
 }
