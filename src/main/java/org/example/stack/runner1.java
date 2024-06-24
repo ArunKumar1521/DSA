3a09cfcaed2429606390c46dfbebf1ceb154d093
Arun <arunkumarb1521@gmail.com>
2024-06-24 13:51:45 +0530

    stack implementation using dynamic-array

--- src/main/java/org/example/stack/runner1.java
+++ src/main/java/org/example/stack/runner1.java
@@ -2,8 +2,20 @@ package org.example.stack;
 
 public class runner1 {
     public static void main(String[] args) {
-        Stack nums = new Stack();
-        nums.show();
+        Dstack nums = new Dstack();
+        nums.push(1);
+        nums.push(11);
+        nums.push(111);
+        nums.push(1111);
+        nums.push(1111);
+        nums.push(111);
+
         nums.pop();
+        nums.pop();
+        nums.pop();
+        nums.pop();
+        nums.pop();
+
+        nums.show();
     }
 }
