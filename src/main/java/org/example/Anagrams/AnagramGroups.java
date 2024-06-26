7565daaff197aef65a734704ff4841eec3dcebdd
Arun <arunkumarb1521@gmail.com>
2024-06-26 22:01:47 +0530

    Anagram code

---
+++ src/main/java/org/example/Anagrams/AnagramGroups.java
@@ -0,0 +1,31 @@
+package org.example.Anagrams;
+
+import java.util.*;
+
+public class AnagramGroups {
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> groupedAnagrams = groupAnagrams(strs);
+        System.out.println(groupedAnagrams);
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new ArrayList<>());
+            }
+            map.get(sorted).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
+
